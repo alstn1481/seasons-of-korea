1ec2ca9f45a3be354d5c55138dc00f0320033ea3
alstn1481 <83435407+alstn1481@users.noreply.github.com>
2024-08-22 11:48:06 +0900

    Add files via upload

---
+++ js/season.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+	// 링크가 이동된 후에 해당 페이지로 ?p0인수를 전달한다.
+	const para = new URLSearchParams(window.location.search); // ?p0 (쿼리스트링부분)
+	//console.log(para.roString() ); // ?p0
+	console.log(i)
+
+	const menu = document.getElementById('menu');
+
+	/* menu.addEventListener("load", () => {
+
+		const nav = menu.contentDocument.querySelectorAll("nav a");
+
+		i.classList.add('act');
+
+	}); */
+
+
+	
+});//////////////////////////전체끝
